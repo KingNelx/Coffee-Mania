b4b6d47ac7602a6a9d7ad13d1997b57e4a208c44
KingNelx <tapiajonel606@gmail.com>
2023-02-22 15:06:13 +0800

    fucking tailwind

--- coffee-mania/src/App.jsx
+++ coffee-mania/src/App.jsx
@@ -1,7 +1,15 @@
+import "./App.css";
+import Home from "./Components/Home";
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+
 const App = () => {
   return ( 
     <div>
-      
+      <Router>
+        <Routes>
+          <Route path="/" element={<Home />} />
+        </Routes>
+      </Router>
     </div>
    );
 }
