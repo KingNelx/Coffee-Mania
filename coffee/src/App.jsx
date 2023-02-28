f77d52d8d6f0b0171c7db64494259da6d8db98a7
KingNelx <tapiajonel606@gmail.com>
2023-02-28 09:17:51 +0800

    added framer

--- coffee/src/App.jsx
+++ coffee/src/App.jsx
@@ -1,13 +1,9 @@
 import "./App.css";
-import Home from "./Components/Home";
-
 
 const App = () => {
   return (
     <div>
-      <nav className="nav" >
-        <Home />
-      </nav>
+    
     </div>
   );
 };
